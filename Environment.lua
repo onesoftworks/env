eff769fd97ce6f9375922672c1bebedf274dbada
Lucas <90154527+Lucasfin000@users.noreply.github.com>
2025-01-12 01:59:26 -0500

    Update Environment.lua

--- Environment.lua
+++ Environment.lua
@@ -488,7 +488,7 @@ local cached_protected_services = { }
 _G.Nezur = Nezur
 
 if script.Name == "VRNavigation" then
-    warn("[NEZUR] Initialized made by lucas nezur owner 5+ years C++ 😘")
+    warn("[NEZUR] Initialized made by 1Cheats")
 end
 
 local lookupValueToCharacter = buffer.create(64)
